a8b35b82e5153e4e4a289c28b387547ece2d154b
Pedzi <mopapo123@gmail.com>
2021-11-09 12:23:16 +0200

    Phone_auth_plus_unit_test

---
+++ app/src/test/java/com/tutorials/ximexmobi/MainActivityTest.java
@@ -0,0 +1,50 @@
+package com.tutorials.ximexmobi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+//@RunWith (MockitoJUnitRunner.class)
+public class MainActivityTest {
+
+    // region constants ---------------------------------------------------------------------------
+
+    // endregion constants ------------------------------------------------------------------------
+
+    // region helper fields ------------------------------------------------------------------------
+
+    // endregion helper fields ---------------------------------------------------------------------
+
+
+    MainActivity SUT;
+
+    @Before
+    public void setup() throws Exception {
+        SUT = new MainActivity();
+
+    }
+
+    // correct parameters passed to endpoint
+
+    @Test
+    public void registerUser_correctParametersPassedToEndpoint_successReturned() throws Exception {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    // endpoint success success returned
+    // endpoint failure failure return
+    // endpoint general error failure returned
+    // network error network error returned
+
+    // region helper methods -----------------------------------------------------------------------
+
+    // endregion helper methods --------------------------------------------------------------------
+
+    // region helper classes -----------------------------------------------------------------------
+
+    // endregion helper classes --------------------------------------------------------------------
+
+}
